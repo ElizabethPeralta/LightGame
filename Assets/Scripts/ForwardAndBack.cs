60c962c9d158f5c60e400917c07684aac9427311
Elizabeth Peralta <perae417@newschool.edu>
2016-02-17 12:22:53 -0500

    Ray cast not working. "simplified platform"

---
+++ Assets/Scripts/ForwardAndBack.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForwardAndBack : MonoBehaviour {
+
+ 
+ 	Vector3 startPosition;
+ 
+ 	void Start () {
+ 		startPosition = transform.position; // remember where we started
+ 	}
+ 	
+ 	// Update is called once per frame
+ 	void Update () {
+ 		transform.position = startPosition 
+ 			+ new Vector3( 0f, Mathf.Sin(Time.time), 0f * 20f);
+ 	}
+ }
