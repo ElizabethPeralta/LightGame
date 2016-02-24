ee89cc4ddfeb259f0d0c00254b30f29ed1408ecd
nick <casen280@newschool.edu>
2016-02-24 13:53:45 -0500

    New Level With Platform

---
+++ Assets/Scripts/trigger01.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trigger01 : MonoBehaviour {
+
+		void OnTriggerEnter(Collider other) {
+
+		   gameObject.SetActive (true);
+			gameObject.tag = "Platform"; 
+
+
+
+	
+	}
+	
+
+}
