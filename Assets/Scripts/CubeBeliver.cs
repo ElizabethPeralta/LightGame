a7d31e9910734801a70da7919dbbd4a98cf33fc4
Elizabeth Peralta <perae417@newschool.edu>
2016-02-29 19:53:27 -0500

    Moving blue platforms.

--- Assets/Scripts/CubeBeliver.cs
+++ Assets/Scripts/CubeBeliver.cs
@@ -1,16 +1,16 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class CubeBeliver : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-		transform.Rotate (0f, CubeGod.instance.godHappiness, 0f);
-	}
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class CubeBeliver : MonoBehaviour {
+//
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//		transform.Rotate (0f, CubeGod.instance.godHappiness, 0f);
+//	}
+//}
