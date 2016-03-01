034589836092a98eafd09dadc9e141b3b2ba7f13
nick <casen280@newschool.edu>
2016-03-01 14:25:18 -0500

    Established platform movement that loops with given distance

---
+++ Assets/Scripts/platform_move02.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platform_move02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	Vector3 floatY;
+	public float number = 0.3f;
+
+	void Update () {
+		floatY = transform.position;
+		floatY.y = (Mathf.Sin(Time.time) * number) + 1.5f;
+		transform.position = floatY;
+	}
+}
