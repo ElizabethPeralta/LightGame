4a41a415294b18637d43ec2f7b5fe7d990d31cfb
nick <casen280@newschool.edu>
2016-03-02 12:38:08 -0500

    Platform Parenting Trigger Addition

---
+++ Assets/Scripts/characterParent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterParent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter( Collider c ){
+
+		if (c.gameObject.GetComponents<CharacterController>() != null ) {
+			c.gameObject.transform.parent = this.gameObject.transform.parent;
+		}
+	}
+
+	void OnTriggerExit( Collider c ){
+		if (c.gameObject.GetComponents<CharacterController> () != null) {
+			c.gameObject.transform.parent = null;
+		}
+
+	}
+}
