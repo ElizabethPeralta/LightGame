2f2819c0c5c126ffe0e7f935303a9fba241ef984
nick <casen280@newschool.edu>
2016-03-02 11:40:35 -0500

    made cyan yellow magenta material

--- Assets/Scripts/platform_move04.cs
+++ Assets/Scripts/platform_move04.cs
@@ -10,6 +10,8 @@ public class platform_move04 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		transform.position = new Vector3(Mathf.PingPong(Time.time,10.0f), transform.position.y, transform.position.z);
 	
 	}
 
