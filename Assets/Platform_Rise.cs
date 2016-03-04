011a7752d7a705f4aaaf1040193cd61edbaa158d
nick <casen280@newschool.edu>
2016-03-03 19:12:40 -0500

    Not working Animator Script added

---
+++ Assets/Platform_Rise.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//public class Platform_Rise : MonoBehaviour {
+//
+//	public Animator myAnimator;
+//
+//	public float amplitude = 0.5f;
+//	public float number = 0.3f;
+//
+//
+//	// Use this for initialization
+//	void Start () {
+//
+//		myAnimator.SetBool ("MakeBridge", false);
+//
+//
+//	}
+//
+//	// Update is called once per frame
+//	void Update () {
+//
+//		if  (myAnimator.SetBool ("MakeBridge", true));{
+//			
+//
+//		
+//
+//
+//
+//		}
+//		if  (myAnimator.SetBool ("MakeBridge", false)); {
+//
+//			gameObject.SetBool ("BlueBridge")
+//			("BlueBridge").SetActive (false);
+//
+//
+//
+//			Vector3 floatY;
+//
+//			floatY = transform.position;
+//			floatY.y = (Mathf.Sin(Time.time * 0.4f) * amplitude) + 1.5f;
+//			transform.position = floatY;
+//		}
+//			
+//		}
+//	}
+//}
