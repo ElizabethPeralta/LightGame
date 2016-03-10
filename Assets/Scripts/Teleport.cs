3b81c9f35fc556ee593960b302e3aaeb11f7579b
faryal <faryalhussain05@hotmail.com>
2016-03-10 16:42:51 -0500

    inventory assets from unity and a working of customizing the inventory

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,35 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class Teleport : MonoBehaviour {
+//	public int Code;
+//	float disableTimer=0;
+//
+//	void Update(){
+//		if (disableTimer > 0)
+//			disableTimer = Time.deltaTime; 
+//	}
+//
+//	void OnTriggerEnter (Collider collider){
+//
+//		if (collider.gameObject.name == "player" && disableTimer<=0) {
+//
+//			foreach(Teleport tp in FindObjectsOfType<Teleport>()){
+//
+//				if(tp.Code==Code && tp ! = this){
+//					tp.disableTimer=2;
+//					Vector3 position=tp.gameObject.transform.position;
+//					position.y+=2;
+//					collider.gameObject.transform.position=position;
+//
+//
+//
+//				}
+//	 		}
+//		}
+//
+//	}
+//
+
+
+
