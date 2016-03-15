3fce6fac8a0272e5129ec8d411030b71824713a7
faryal <faryalhussain05@hotmail.com>
2016-03-14 21:41:19 -0400

    added box triggers that open the doors.  scene "LeversOpeningDoors"

---
+++ Assets/Scripts/FaryalScripts/Doors.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Doors : MonoBehaviour {
+
+	public Animator animator; 
+	bool doorOpen; 
+
+	void Start()
+	{
+		doorOpen = false;
+		//animator = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player") 
+		
+		{
+			doorOpen = true; 
+			DoorsControl("Open");
+		}
+
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (doorOpen) 
+		{
+			doorOpen = false;
+			DoorsControl ("Close");
+		}
+
+
+	}
+
+
+	void DoorsControl(string direction)
+
+	{
+		animator.SetTrigger(direction);
+	}
+}
