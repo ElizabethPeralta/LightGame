d3f63eba15824dfdfe13852136edb2442c2cfae0
faryal <faryalhussain05@hotmail.com>
2016-03-16 11:16:57 -0400

    color shift lighting, teleportation pads and doors animation working in the same scene. "LeversOpeningDoors"

--- Assets/Scripts/FaryalScripts/ItemDatabase.cs
+++ Assets/Scripts/FaryalScripts/ItemDatabase.cs
@@ -1,17 +1,17 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-
-public class ItemDatabase : MonoBehaviour {
-	public List <Items> items = new List <Items> (); 
-
-	void Start ()
-
-	{
-		items.Add (new Items ("lever",0, "a lever that you have to pull",2,0,Items.ItemType.Lever));
-	}
-
-
- 
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//
+//
+//public class ItemDatabase : MonoBehaviour {
+//	public List <Items> items = new List <Items> (); 
+//
+//	void Start ()
+//
+//	{
+//		items.Add (new Items ("lever",0, "a lever that you have to pull",2,0,Items.ItemType.Lever));
+//	}
+//
+//
+// 
+//}
