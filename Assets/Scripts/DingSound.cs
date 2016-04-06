a8f241a6ab7b1e5f2336171f2e911323503e8413
faryal <faryalhussain05@hotmail.com>
2016-04-05 22:42:46 -0400

    gray boxed 3 rooms "fh_graybox_1"

--- Assets/Scripts/DingSound.cs
+++ Assets/Scripts/DingSound.cs
@@ -1,20 +1,20 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class NewBehaviourScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-		audio = GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-
-	
-		void OnCollisionEnter(collision : Collision) {
-			if (collision)
-				audio.Play();
-		}
-	}
-
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class NewBehaviourScript : MonoBehaviour {
+//
+//	// Use this for initialization
+//	void Start () {
+//	
+//		audio = GetComponent<AudioSource>();
+//	}
+//	
+//	// Update is called once per frame
+//
+//	
+//		void OnCollisionEnter(collision : Collision) {
+//			if (collision)
+//				audio.Play();
+//		}
+//	}
+//
