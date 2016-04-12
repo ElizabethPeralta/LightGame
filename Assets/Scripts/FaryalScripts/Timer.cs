2244caa0caef8e9724cac2c8ef332ea90124d998
faryal <faryalhussain05@hotmail.com>
2016-04-11 20:09:30 -0400

    lighting and reflection with water "chapelLighting" with an example of particle effects

---
+++ Assets/Scripts/FaryalScripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer: MonoBehaviour {
+
+	public float targetTime = 60.0f;
+
+	void Update(){
+
+		targetTime -= Time.deltaTime;
+
+		if (targetTime <= 0.0f)
+		{
+			timerEnded();
+		}
+
+	}
+
+	void timerEnded()
+	{
+		//do your stuff here.
+	}
+
+
+}
---
+++ Assets/trailMoveScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trailMoveScript : MonoBehaviour {
+
+	public float moveForce;
+	public float jumpforce;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float h = Input.GetAxis ("Horizontal") * moveForce;
+		float v = Input.GetAxis ("Verical") * moveForce;
+
+		GetComponent<Rigidbody>().AddForce (new Vector3 (h, 0, v));
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody>().AddForce (Vector3.up * jumpforce);
+
+		}
+			
+
+
+	}
+}
