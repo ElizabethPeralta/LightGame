7e90c945dbe0d7c57d76627ac167d7ee8118e46b
Elizabeth Peralta <perae417@newschool.edu>
2016-04-28 21:02:36 -0400

    script "SlowFloatingObject"

--- Assets/Scripts/FaryalScripts/floatingObject.cs
+++ Assets/Scripts/FaryalScripts/floatingObject.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class floatingObject : MonoBehaviour {
+public class SlowFloatingObject : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
