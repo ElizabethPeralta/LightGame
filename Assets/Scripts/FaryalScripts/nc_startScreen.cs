0bc018983e13dd1cda22345ff942b411f9f0621a
nick <casen280@newschool.edu>
2016-05-09 19:35:05 -0400

    edited crystals and terrain exploration

---
+++ Assets/Scripts/FaryalScripts/nc_startScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class nc_startScreen : MonoBehaviour {
+
+	// Use this for initialization
+	 
+
+	public void StartLevel()
+	{
+		SceneManager.LoadScene ("nc_duplicate");
+
+	}
+}
