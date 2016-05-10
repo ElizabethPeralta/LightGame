e3d830c17eb208abb76185d1b2ae2f17c09e896c
faryal <faryalhussain05@hotmail.com>
2016-05-10 19:47:02 -0400

    new capsule "bestCapsuleEverWithSound"

---
+++ Assets/Scripts/FaryalScripts/ambeintSoundTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class  ambeintSoundTrigger : MonoBehaviour {
+
+	void OnTriggerEnter( Collider col) {
+
+	
+
+		if (col.tag == "Player")
+		{
+			 
+ 	 		GetComponent<AudioSource> ().Play ();
+		}
+
+
+	}
+
+}
