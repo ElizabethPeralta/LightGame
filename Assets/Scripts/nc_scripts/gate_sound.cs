2fca59cfd415b96d37b4a99434e0f7310b2f9611
nick <casen280@newschool.edu>
2016-05-10 19:12:24 -0400

    nc_duplicate working scene

---
+++ Assets/Scripts/nc_scripts/gate_sound.cs
@@ -0,0 +1,19 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class gate_sound : MonoBehaviour {
+//	
+//
+//	// Use this for initialization
+//	void OnTriggerEnter (Collider gameobject)
+//
+//		if (gameobject.tag == "player")
+//		{
+//			other.audio.Play ();
+//	
+//	}
+//		else {
+//			audio.Stop ();
+//	
+//	
+//}
