50a57d4670e0b374d2356f77e9b928b71a47ce30
faryal <faryalhussain05@hotmail.com>
2016-05-09 20:03:58 -0400

    newPrefab for timeCapsule "BestCapsuleEver"

---
+++ Assets/Scene/NickScenes/nick_scenes2/FinalUltimateBestTriggerScriptEver.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalUltimateBestTriggerScriptEver : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	public Renderer crystal;
+	public Animator anim;
+
+
+	void Start () {
+		//anim.enabled = false;
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.tag == "Player"){
+			
+		anim.Play ("payOffParticles");
+			crystal.enabled = false; 
+
+		//anim.enabled = true;
+		//anim.GetComponent <ParticleSystem>().Play();
+			Destroy(transform.parent.parent.gameObject,1.5f);}
+	}
+
+	void OnDestroy(){ 
+		anim.Play ("defaultStatePayoff");
+		//anim.enabled = false;
+	} 
+
+}
